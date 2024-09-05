db8297443ec7f732dd21e917beb99f85da2a15f7
Jeremy Yeo <jeremyyeo@gmail.com>
2024-09-06 09:56:04 +1200

    update

--- macros/run_a_query.sql
+++ macros/run_a_query.sql
@@ -1,3 +1,6 @@
 {% macro run_a_query() %}
-    select 1 from {{ var('from_where', 'this_doesnt_exist') }}
+    {% set query %}
+        select 1 from {{ var('from_where', 'this_doesnt_exist') }}
+    {% endset %}
+    {% do run_query(query) %}
 {% endmacro %}
